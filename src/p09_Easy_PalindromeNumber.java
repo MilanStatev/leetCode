ef3744d2d72585610cad3e4b188c7c99c3df2104
Milan <milan_statev@yahoo.co.uk>
2025-03-25 19:50:28 +0200

    Add PalindromeNumber problem solution 5ms

--- src/p09_Easy_PalindromeNumber.java
+++ src/p09_Easy_PalindromeNumber.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class p09_Easy_PalindromeNumber {
+    public static void main(String[] args) {
+        long input = 9987654321L;
+
+        System.out.println(isPalindrome(input));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String numberToString = String.valueOf(x);
+        for (int i = 0; i < numberToString.length() / 2; i++) {
+            if (numberToString.charAt(i) != numberToString.charAt(numberToString.length() - 1 - i)) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindrome(long x) {
+        if (x < 0) return false;
+
+        long original = x;
+        long reverse = 0;
+
+        do {
+            reverse = reverse * 10 + (x % 10);
+            x /= 10;
+        } while (x != 0);
+
+        return reverse == original;
+    }
 }
